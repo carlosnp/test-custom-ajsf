fc5144a66cf2ab39ff3c8c9b6dde1d57e0397c58
Carlos Nava <machinery.e@gmail.com>
2022-06-01 17:15:46 -0400

    Agregar libreria de formularios

--- src/app/future/components/home-form/home-form.component.ts
+++ src/app/future/components/home-form/home-form.component.ts
@@ -9,6 +9,12 @@ import {
   Subject
 } from 'rxjs';
 
+import {
+  layout,
+  options,
+  schemaBasic
+} from './schemas';
+
 @Component({
   selector: 'app-home-form',
   templateUrl: './home-form.component.html',
@@ -20,10 +26,37 @@ export class HomeFormComponent implements OnInit, OnDestroy {
    * Subject para manejar destrucción del objeto
    */
   componentDestroyed$: Subject<void> = new Subject<void>();
-
+  schema = schemaBasic;
+  layout = layout;
+  options = options;
+  data = {};
+  res = null;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  /**
+   * Cargar datos de prueba
+   */
+  chargeData() {
+    this.data = {
+      "name": "Gianni Lockman",
+      "email": "your.email+fakedata52899@gmail.com",
+      "comment": "Jasmin Wisoky"
+    }
+  }
+  /**
+   * Borrar datos del formulario
+   */
+  reset() {
+    this.data = {};
+  }
+  /**
+   * Formulario
+   * @param $event
+   */
+  handleSubmit($event: any) {
+    this.res =  $event;
   }
   /**
    * Destructor del componente
---
+++ src/app/future/components/home-form/schemas/options.ts
@@ -0,0 +1,6 @@
+/**
+ * Opciones
+ */
+export const options = {
+
+};
